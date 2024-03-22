e2ae74a03f8cb85d16cc78998548e0ca5191d384
tioon <59834576+tioon@users.noreply.github.com>
2024-03-22 22:22:19 +0900

    refactor : WSGI 배포 방식 변경

--- prompt_gpt.py
+++ prompt_gpt.py
@@ -745,5 +745,8 @@ def prompt6():
         return jsonify({"error": str(e)}), 400
 
 
+
 if __name__ == '__main__':
-    app.run(port=5000)
+    from waitress import serve
+    print("server stating....")
+    serve(app, host='0.0.0.0', port=5050)
